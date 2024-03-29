204a807734a9dba1a45bad5dddd6244107a26f57
mayankthakur0007 <161581465+Vastwragger@users.noreply.github.com>
2024-03-29 17:16:00 +0530

    ui

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -7,16 +7,19 @@ const BillboardPage = async ({
 }: {
   params: { billboardId: string }
 }) => {
-  // const billboard = await prismadb.billboard.findUnique({
-  //   where: {
-  //     id: params.billboardId
-  //   }
-  // });
 
+  let billboard = null;
+  if (params.billboardId != "new") {
+    billboard = await prismadb.banner.findUnique({
+      where: {
+        id: params.billboardId,
+      },
+    });
+  }
   return ( 
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardForm initialData={null} />
+        <BillboardForm initialData={billboard} />
       </div>
     </div>
   );
