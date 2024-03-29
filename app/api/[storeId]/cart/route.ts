a40886a0946d09cca52f5a53bed7d8ac6f01ae13
mayankthakur0007 <161581465+Vastwragger@users.noreply.github.com>
2024-03-30 02:20:57 +0530

    new cart count api

--- app/api/[storeId]/cart/route.ts
+++ app/api/[storeId]/cart/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import prismadb from '@/lib/prismadb';
 import { decodeToken, signToken } from '@/app/_utils/AuthUtil';
+import { Qahiri } from 'next/font/google';
 
 export async function POST(
   req: Request
@@ -136,6 +137,60 @@ export async function GET(req:Request) {
   if(!user?.cartId)  return NextResponse.json({cart:[], status: 200 });
   return NextResponse.json({ cart:user?.cart, status: 200 });
 }
+
+export async function PATCH(
+  req: Request
+) {
+  try {   
+    const body = await req.json();
+    
+    const { data } = body;
+    let quantity =
+      data.step == "increase"
+        ? Number(data.quantity) + 1
+        : Number(data.quantity) - 1;    
+    
+    await prismadb.cartItem.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        quantity: JSON.stringify(quantity),
+      },
+    });
+    let userToken = decodeToken(req);
+    const user = await prismadb.user.findUnique({
+      where: {
+        id: userToken.user_id,
+      },
+      include: {
+        cart: {
+          include: {
+            cartItem: {
+              include: {
+                color: true,
+                size: true,
+                product: {
+                  include: {
+                    images: true,
+                    category: true,
+                    color: true,
+                    size: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });  
+    if(!user?.cartId)  return NextResponse.json({cart:[], status: 200 });
+    return NextResponse.json({ cart:user?.cart, status: 200 });  
+  } catch (error) {
+    console.log('[BANNER_PATCH]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
 
 export async function OPTIONS(req: Request) {
   try {
